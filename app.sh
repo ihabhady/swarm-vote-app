5d62e9ea1118b9bc2c4d319fa252aebdeb8301df
ihab-hady <52742729+ihab-hady@users.noreply.github.com>
2022-03-12 13:13:35 +0200

    Update app.sh

--- app.sh
+++ app.sh
@@ -14,7 +14,12 @@
  docker stack deploy -c swarm-autoscaler-stack.yml autoscaler
  cd /home/vagrant/example-voting-app
  docker stack deploy --compose-file docker-stack-01.yml vote
-
+  echo "============== scaling up vote app services ====================="
+ docker service scale vote_db=1
+ docker service scale vote_redis=1
+ docker service scale vote_worker=1
+ docker service scale vote_visualizer=1
+ docker service scale vote_result=1
 '
 
 
