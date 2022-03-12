d5bff5db375857a87afd3d9c75bea28426471e4f
ihab-hady <52742729+ihab-hady@users.noreply.github.com>
2022-03-12 12:42:01 +0200

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,11 +4,11 @@
  brew  install --cask virtualbox
  brew  install --cask vagrant
  brew  install --cask vagrant-manager
- #vagrant box add ubuntu/xenial64 http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box
+ vagrant box add ubuntu/xenial64 http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box
  vagrant up
  OUTPUT= vagrant global-status | grep manager
- #vagrant ssh $OUTPUT  -- -t '
- vagrant ssh 1399da8  -- -t '
+ vagrant ssh $OUTPUT  -- -t '
+ #vagrant ssh 1399da8  -- -t '
  git clone https://github.com/jcwimer/docker-swarm-autoscaler.git
  cd docker-swarm-autoscaler
  docker stack deploy -c swarm-autoscaler-stack.yml autoscaler
